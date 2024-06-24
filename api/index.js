30822ca77c0ee59199cf85e668ec5b15eda0fef5
Uheeking <a@infob.co.kr>
2024-06-24 09:21:17 +0900

    FIX : moved the file

--- api/index.js
+++ api/index.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const swaggerFile = require("./swaggerFile/swagger-output.json");
+const swaggerUi = require("swagger-ui-express");
+require("dotenv").config();
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = ["http://localhost:3000", "https://question-for.vercel.app/"]; // Add other allowed origins if needed
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const PORT = process.env.PORT;
+app.set("port", process.env.PORT || 3002);
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.get("/", (req, res) => {
+  res.send("Hello, Express");
+});
+app.use("/api", require("./api/question"));
+app.use("/api", require("./api/like"));
+app.use("/api/oauth", require("./api/oauth"));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile, { explorer: true })
+);
+mongoose
+  .connect(process.env.DB,  {})
+  .then(() => console.log("connect to database"));
+
+app.listen(PORT, () => {
+  console.log(PORT, "번 포트에서 대기 중");
+});
 
 module.exports = app;
