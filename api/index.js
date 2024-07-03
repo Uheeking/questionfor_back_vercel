a1c56ebdf991e78cffb4cbc97d196069f7fedbdb
Uheeking <a@infob.co.kr>
2024-07-03 14:44:41 +0900

    MODIFY : api docs 수정

--- api/index.js
+++ api/index.js
@@ -3,8 +3,6 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const swaggerFile = require("./swaggerFile/swagger-output.json");
-const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
 app.use(bodyParser.json());
 app.use(
@@ -28,15 +26,9 @@ app.set("port", process.env.PORT || 3002);
 app.get("/", (req, res) => {
   res.send("Hello, Express");
 });
-// app.use("/api/test", require("./api/test"));
-app.use("/api", require("./api/question"));
-app.use("/api", require("./api/like"));
-app.use("/api/oauth", require("./api/oauth"));
-// app.use(
-//   "/api-docs",
-//   swaggerUi.serve,
-//   swaggerUi.setup(swaggerFile, { explorer: true })
-// );
+app.use("/api", require("./question"));
+app.use("/api", require("./like"));
+app.use("/api/oauth", require("./oauth"));
 mongoose
   .connect(process.env.DB,  {})
   .then(() => console.log("connect to database"));
