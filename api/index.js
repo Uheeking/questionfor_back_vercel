36ffb51691f1739800eb203b154ffac7a11765c4
Uheeking <a@infob.co.kr>
2024-07-03 14:35:56 +0900

    FIX : cors error

--- api/index.js
+++ api/index.js
@@ -22,16 +22,6 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
-var whitelist = ["http://localhost:3000", "https://question-for.vercel.app"];
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
 
 
 const PORT = process.env.PORT || 3002;
