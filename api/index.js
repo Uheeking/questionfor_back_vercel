b748725834ba4de75f83312112f571c7e06d2bc2
Uheeking <a@infob.co.kr>
2024-07-03 14:29:17 +0900

    FIX : data 500 error

--- api/index.js
+++ api/index.js
@@ -32,11 +32,7 @@ var corsOptionsDelegate = function (req, callback) {
   }
   callback(null, corsOptions) // callback expects two parameters: error and options
 }
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.header('Origin'));
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
+
 
 const PORT = process.env.PORT || 3002;
 
