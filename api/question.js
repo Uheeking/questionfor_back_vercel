713e4cebb0867aaa6d09f0f34d697ecfa8723870
Uheeking <wert1847@naver.com>
2024-07-14 21:28:23 +0900

    test 추가

--- api/question.js
+++ api/question.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const QuestionItem = require("../Models/question");
-// router.post("/", async (req, res) => {
-//   try {
-//     const question = new QuestionItem(req.body);
-//     const savedQuestion = await question.save();
-//     res.json(savedQuestion);
-//   } catch (error) {
-//     res.status(500).json({ error: "Could not create a new question." });
-//   }
-// });
-router.get("/", async (req, res) => {
-  res.send("please show me your self");
+console.log("추가");
+
+router.post("/", async (req, res) => {
+  try {
+    const question = new QuestionItem(req.body);
+    const savedQuestion = await question.save();
+    res.json(savedQuestion);
+  } catch (error) {
+    res.status(500).json({ error: "Could not create a new question." });
+  }
 });
 
 router.get("/question", async (req, res) => {
@@ -23,25 +22,25 @@ router.get("/question", async (req, res) => {
   }
 });
 
-// router.patch("/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { answer } = req.body;
-//     const existingQuestion = await QuestionItem.findOne({ _id: id });
+router.patch("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { answer } = req.body;
+    const existingQuestion = await QuestionItem.findOne({ _id: id });
 
-//     if (existingQuestion) {
-//       await QuestionItem.updateOne({ _id: id }, { answer });
-//       res.json({ message: "Question updated successfully." });
-//     }
-//   } catch (error) {
-//     console.error("Error adding question:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
+    if (existingQuestion) {
+      await QuestionItem.updateOne({ _id: id }, { answer });
+      res.json({ message: "Question updated successfully." });
+    }
+  } catch (error) {
+    console.error("Error adding question:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-// router.delete("/:id", async (req, res) => {
-//   await QuestionItem.deleteOne({ _id: req.params.id });
-//   res.json({ message: "question deleted." });
-// });
+router.delete("/:id", async (req, res) => {
+  await QuestionItem.deleteOne({ _id: req.params.id });
+  res.json({ message: "question deleted." });
+});
 
 module.exports = router;
--- api/test.js
+++ api/test.js
@@ -2,13 +2,44 @@ const express = require("express");
 const router = express.Router();
 const QuestionItem = require("../Models/question");
 
-router.get("/question", async (req, res) => {
+router.post("/", async (req, res) => {
+  try {
+    const question = new QuestionItem(req.body);
+    const savedQuestion = await question.save();
+    res.json(savedQuestion);
+  } catch (error) {
+    res.status(500).json({ error: "Could not create a new question." });
+  }
+});
+
+router.get("/", async (req, res) => {
   try {
     const question = await QuestionItem.find();
     res.json(question);
   } catch (err) {
     res.status(500).json({ error: "Could not retrieve question." });
   }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { answer } = req.body;
+    const existingQuestion = await QuestionItem.findOne({ _id: id });
+
+    if (existingQuestion) {
+      await QuestionItem.updateOne({ _id: id }, { answer });
+      res.json({ message: "Question updated successfully." });
+    }
+  } catch (error) {
+    console.error("Error adding question:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  await QuestionItem.deleteOne({ _id: req.params.id });
+  res.json({ message: "question deleted." });
 });
 
 module.exports = router;
