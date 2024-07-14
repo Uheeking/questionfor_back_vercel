d86afcfb381e0844ddc8b9177ed709f3d2172c07
Uheeking <wert1847@naver.com>
2024-07-14 21:13:11 +0900

    test question 추가

--- api/test.js
+++ api/test.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const QuestionItem = require("../Models/question");
 
 router.get("/", async (req, res) => {
   res.send("please show me your self");
+});
+
+router.get("/question", async (req, res) => {
+  try {
+    const question = await QuestionItem.find();
+    console.log("please,,,,,");
+    res.json(question);
+  } catch (err) {
+    res.status(500).json({ error: "Could not retrieve question." });
+  }
 });
 
 module.exports = router;
