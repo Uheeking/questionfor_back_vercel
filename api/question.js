bf8011bcf7871d3c54aa3cd2c3451fcd8e1e7930
Uheeking <a@infob.co.kr>
2024-07-15 09:58:49 +0900

    ADD : error를 위한 로그 설정

--- api/question.js
+++ api/question.js
@@ -16,8 +16,10 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const question = await QuestionItem.find();
+    console.log(question);
     res.json(question);
   } catch (err) {
+    console.log(err);
     res.status(500).json({ error: "Could not retrieve question." });
   }
 });
